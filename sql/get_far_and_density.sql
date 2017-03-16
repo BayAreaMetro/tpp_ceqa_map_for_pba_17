bb45f4417a1f3ca384ed1ea4cd5d1992d75c7974
Tom Buckley <buckley.tom@gmail.com>
2017-03-16 15:03:33 -0700

    add in mixed use
    for https://github.com/MetropolitanTransportationCommission/tpp_ceqa_map_for_pba_17/issues/7

--- sql/get_far_and_density.sql
+++ sql/get_far_and_density.sql
@@ -5,9 +5,11 @@
 --https://github.com/MetropolitanTransportationCommission/bayarea_urbansim/blob/master/baus/variables.py#L786
 --unclear if this includes common area. will assume that it does. 
 
-create view UrbanSim.Parcels_Building_Square_Footage as
+create view UrbanSim.Parcels_CEQA_Square_Footage as
 SELECT  b.parcel_id,
-		sum(b.residential_units)*1000 as estimated_residential_square_feet
+		sum(b.non_residential_sqft) as total_non_residential_sqft,
+		sum(b.residential_units)*1000 as total_residential_sqft,
+		((sum(b.residential_units)*1000) + sum(b.non_residential_sqft)*sum(b.residential_units)/(abs(sum(b.residential_units)-1)+1)) as estimated_ceqa_square_feet -- from http://puzzling.stackexchange.com/questions/33625/function-that-is-1-for-all-positive-integers-but-0-at-0
   FROM  DEIR2017.UrbanSim.RUN7224_BUILDING_DATA_2040 as b
 		group by b.parcel_id;
 
@@ -16,23 +18,23 @@ SELECT  b.parcel_id,
 
 CREATE VIEW UrbanSim.Parcels_FAR_Units_Per_Acre AS
 SELECT  (CASE WHEN p.acres = 0 THEN NULL 
-			ELSE pb.estimated_residential_square_feet /  
+			ELSE pb.estimated_ceqa_square_feet /  
 			(p.acres*43560) END) as far_estimate,
 		(CASE WHEN p.acres = 0 THEN NULL 
 			ELSE Y2040.total_residential_units/p.acres 
 			END) as units_per_acre,
-		pb.estimated_residential_square_feet as est_res_sq_ft,
+		pb.estimated_ceqa_square_feet as est_ceqa_sq_ft,
 		p.PARCEL_ID,
 		p.tpa_objectid,
 		p.taz_id,
 		p.superd_id
-  FROM  DEIR2017.UrbanSim.Parcels_Building_Square_Footage as pb JOIN
+  FROM  DEIR2017.UrbanSim.Parcels_CEQA_Square_Footage as pb JOIN
 		DEIR2017.UrbanSim.Parcels as p ON pb.parcel_id = p.parcel_id JOIN
 		UrbanSim.RUN7224_PARCEL_DATA_2040 AS y2040 ON p.PARCEL_ID = y2040.parcel_id;
 GO
 
 CREATE VIEW UrbanSim.Parcels_FAR_Units_Per_Acre_Non_Zero_Drop_Small AS
-SELECT  pb.est_res_sq_ft,
+SELECT  pb.est_ceqa_sq_ft,
 		pb.units_per_acre,
 		pb.far_estimate,
 		p.OBJECTID,
