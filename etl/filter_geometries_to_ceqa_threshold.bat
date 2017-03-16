77f3301eaeb684b01efe6292ebfec4a91ad8f028
Tom Buckley <buckley.tom@gmail.com>
2017-03-16 12:35:46 -0700

    small query fixup-final touches

--- etl/filter_geometries_to_ceqa_threshold.bat
+++ etl/filter_geometries_to_ceqa_threshold.bat
@@ -4,29 +4,26 @@ set myquery=select far_q8_prcnt, ^
 far_cnt, ^
 ua_q8, ^
 ua_cnt, ^
-taz_tpa_geometry ^
-from tpa_clipped_taz ^
-WHERE ua_q8>20
+taz_id ^
+from tpa_clipped_taz
 
 ogr2ogr ^
 -append ^
 -f "FileGDB" ^
 -nln tpa_clipped_taz_ua_filter ^
--sql "%myquery%" ^
-data/taz_ua_far_quantiles.gdb data/tpp_ceqa_pb_17.gdb
+-sql "%myquery% WHERE ua_q8 > 20" ^
+data/taz_ua_far_quantiles.gdb data/taz_ua_far_quantiles.gdb
 
 set myquery2=select far_q8_prcnt, ^
 far_cnt, ^
 ua_q8, ^
 ua_cnt, ^
-taz_tpa_geometry ^
-from tpa_clipped_taz ^
-WHERE ua_q8>20 ^
-AND far_q8_prcnt>0.75
+taz_id ^
+from tpa_clipped_taz
 
 ogr2ogr ^
 -append ^
 -f "FileGDB" ^
 -nln tpa_clipped_taz_ua_far_filter ^
--sql "%myquery%" ^
-data/taz_ua_far_quantiles.gdb data/tpp_ceqa_pb_17.gdb
+-sql "%myquery2% WHERE ua_q8 > 20 AND far_q8_prcnt > 75" ^
+data/taz_ua_far_quantiles.gdb data/taz_ua_far_quantiles.gdb
